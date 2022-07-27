1fad6cc8362635fb3fd2b3d7e6abb397983dc2ad
imnikhilanand <imnikhilanand@gmail.com>
2022-07-26 23:54:20 -0400

    Added Kaplan-Meier survival estimate

--- analysis.R
+++ analysis.R
@@ -12,3 +12,50 @@ data(diabetic, package='survival')
 
 # displaying the top few rows of the diabetic dataset
 head(diabetic)
+
+# columns in the diabetic dataframe
+names(diabetic)
+
+# only those rows where patient was diagonised
+diabetic2 <- subset(diabetic, trt==1)
+
+# fitting the survival curve
+fit<-survfit(Surv(time, status) ~ laser, data=diabetic2)
+
+print(fit)
+
+# Summary of survival curves
+summary(fit)
+
+# Access to the sort summary table
+summary(fit)$table
+
+d <- data.frame(time = fit$time,
+                n.risk = fit$n.risk,
+                n.event = fit$n.event,
+                n.censor = fit$n.censor,
+                surv = fit$surv,
+                upper = fit$upper,
+                lower = fit$lower
+)
+
+
+head(d)
+
+# Visualizing the Kaplan-Meire Curve
+ggsurvplot(fit,
+           pval = TRUE, conf.int = TRUE,
+           risk.table = TRUE, # Add risk table
+           risk.table.col = "strata", # Change risk table color by groups
+           linetype = "strata", # Change line type by groups
+           surv.median.line = "hv", # Specify median survival
+           ggtheme = theme_bw(), # Change ggplot2 theme
+           palette = c("#E7B800", "#2E9FDF"))
+
+
+
+
+
+
+
+
